1c5f7f26bfb20c7326fb111625d7e7c6597f37f5
Your Name <you@example.com>
2022-02-07 23:37:38 +0000

    feat: add page_not_found

---
+++ front/src/page_not_found/mod.rs
@@ -0,0 +1,4 @@
+pub(crate) mod page_not_found;
+
+#[cfg(test)]
+mod test_page_not_found;
---
+++ front/src/page_not_found/page_not_found.rs
@@ -0,0 +1,21 @@
+use gloo_utils::{document};
+use yew::prelude::*;
+
+const HTML_PATH: &str = include_str!("page_not_found.html");
+
+pub struct PageNotFoundComponent;
+impl Component for PageNotFoundComponent {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        return Self;
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let div = document().create_element("div").unwrap();
+        div.set_inner_html(HTML_PATH);
+
+        return Html::VRef(div.into());
+    }
+}
