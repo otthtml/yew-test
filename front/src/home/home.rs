0870199e5205943b6bafdcdcab4e317b6d9254a6
Your Name <you@example.com>
2022-02-07 23:36:27 +0000

    feat: add home page

---
+++ front/src/home/home.rs
@@ -0,0 +1,21 @@
+use gloo_utils::{document};
+use yew::prelude::*;
+
+const HTML_PATH: &str = include_str!("home.html");
+
+pub struct HomeComponent;
+impl Component for HomeComponent {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        return Self;
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        let div = document().create_element("div").unwrap();
+        div.set_inner_html(HTML_PATH);
+
+        return Html::VRef(div.into());
+    }
+}
---
+++ front/src/home/mod.rs
@@ -0,0 +1,4 @@
+pub(crate) mod home;
+
+#[cfg(test)]
+mod test_home;
